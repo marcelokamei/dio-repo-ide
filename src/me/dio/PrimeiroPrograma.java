36f2c0965627ac55f2b4712b22664d6e132e6e63
marcelokamei <marcelokamei@hotmail.com>
2022-01-12 23:42:45 -0400

    Segundo commit

--- src/me/dio/PrimeiroPrograma.java
+++ src/me/dio/PrimeiroPrograma.java
@@ -9,8 +9,8 @@ public class PrimeiroPrograma {
 		 * int a = 2; int b = 9; System.out.println("Alou mundo! " + (a+b));
 		 */
 
-		Gato gato = new Gato("Bob", "Branco", 3);
-		Livros livros = new Livros("Joana Dark", 250);
+		Gato gato = new Gato("Bob", "Preto", 5);
+		Livros livros = new Livros("Machado de Assis", 250);
 
 		System.out.println(gato);
 		System.out.println(livros);
